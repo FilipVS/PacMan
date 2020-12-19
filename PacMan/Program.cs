73d939c313a56e2100ffa2d48aae38553893101f
FilipVS <setnicka32@gmail.com>
2020-12-19 09:11:35 +0100

    Initial commit  Changes to be committed: 	new file:   PacMan.sln 	new file:   PacMan/PacMan.csproj 	new file:   PacMan/Program.cs

---
+++ PacMan/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Setnicka.PacMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
